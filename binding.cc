e83dc00cf8171b9794bcc6fd24efd0d19882c156
Tony Gorez <gorez.tony@gmail.com>
2025-05-23 12:52:43 +0100

    feat(window): add base
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- binding.cc
+++ binding.cc
@@ -1,4 +1,6 @@
+#include "SDL3/SDL_video.h"
 #include <bare.h>
+#include <cstdint>
 #include <js.h>
 #include <jstl.h>
 
@@ -6,18 +8,55 @@
 
 typedef struct {
   SDL_Window *handle;
-} bare_sdl2_window_t;
+} bare_sdl_window_t;
+
+static js_arraybuffer_t
+bare_sdl_create_window(
+  js_env_t *env,
+  js_receiver_t,
+  js_string_t title,
+  uint32_t width,
+  uint32_t height
+) {
+  int err;
+  js_arraybuffer_t handle;
+
+  std::string value;
+  err = js_get_value_string(env, title, value);
+  assert(err == 0);
+
+  bare_sdl_window_t *win;
+  err = js_create_arraybuffer(env, sizeof(bare_sdl_window_t), win, handle);
+  assert(err == 0);
+
+  win->handle = SDL_CreateWindow(value.c_str(), width, height, 0);
+  assert(win->handle != nullptr);
+
+  return handle;
+}
 
 static js_value_t *
 bare_sdl_exports(js_env_t *env, js_value_t *exports) {
   int err;
   js_object_t _exports = static_cast<js_object_t>(exports);
 
+  // TODO: move that away
+  SDL_Init(SDL_INIT_VIDEO);
+
 #define V_UINT32(name, constant) \
   err = js_set_property(env, _exports, name, static_cast<uint32_t>(constant)); \
   assert(err == 0);
 
+#define V_FUNCTION(name, function) \
+  err = js_set_property<function>(env, _exports, name);
+  assert(err == 0);
+
   V_UINT32("SDL_WINDOWPOS_CENTERED", SDL_WINDOWPOS_CENTERED)
+
+  V_FUNCTION("createWindow", bare_sdl_create_window)
+
+#undef V_UINT32
+#undef V_FUNCTION
 
   return exports;
 }
--- test.js
+++ test.js
@@ -4,3 +4,8 @@ const sdl2 = require('.')
 test('it should expose window position center const', (t) => {
   t.ok(sdl2.constants.SDL_WINDOWPOS_CENTERED)
 })
+
+test('it should expose a window class', (t) => {
+  const win = new sdl.Window('Bare-ly a Window', 100, 100)
+  t.ok(win)
+})
