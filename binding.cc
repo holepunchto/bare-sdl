13e8471ae1ec1171438e241358571278b198c639
Tony Gorez <gorez.tony@gmail.com>
2025-05-26 18:23:39 +0100

    feat(window): add flags
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- binding.cc
+++ binding.cc
@@ -22,7 +22,8 @@ bare_sdl_create_window(
   js_receiver_t,
   std::string title,
   uint32_t width,
-  uint32_t height
+  uint32_t height,
+  uint64_t flags
 ) {
   int err;
   js_arraybuffer_t handle;
@@ -31,7 +32,7 @@ bare_sdl_create_window(
   err = js_create_arraybuffer(env, win, handle);
   assert(err == 0);
 
-  win->handle = SDL_CreateWindow(title.c_str(), width, height, 0);
+  win->handle = SDL_CreateWindow(title.c_str(), width, height, flags);
   if (win->handle == nullptr) {
     err = js_throw_error(env, NULL, SDL_GetError());
     assert(err == 0);
@@ -60,7 +61,30 @@ bare_sdl_exports(js_env_t *env, js_value_t *exports) {
   err = js_set_property(env, _exports, name, static_cast<uint32_t>(constant)); \
   assert(err == 0);
 
-  V("SDL_WINDOWPOS_CENTERED", SDL_WINDOWPOS_CENTERED)
+  V("SDL_WINDOW_FULLSCREEN", SDL_WINDOW_FULLSCREEN)
+  V("SDL_WINDOW_OPENGL", SDL_WINDOW_OPENGL)
+  V("SDL_WINDOW_OCCLUDED", SDL_WINDOW_OCCLUDED)
+  V("SDL_WINDOW_HIDDEN", SDL_WINDOW_HIDDEN)
+  V("SDL_WINDOW_BORDERLESS", SDL_WINDOW_BORDERLESS)
+  V("SDL_WINDOW_RESIZABLE", SDL_WINDOW_RESIZABLE)
+  V("SDL_WINDOW_MINIMIZED", SDL_WINDOW_MINIMIZED)
+  V("SDL_WINDOW_MAXIMIZED", SDL_WINDOW_MAXIMIZED)
+  V("SDL_WINDOW_MOUSE_GRABBED", SDL_WINDOW_MOUSE_GRABBED)
+  V("SDL_WINDOW_INPUT_FOCUS", SDL_WINDOW_INPUT_FOCUS)
+  V("SDL_WINDOW_MOUSE_FOCUS", SDL_WINDOW_MOUSE_FOCUS)
+  V("SDL_WINDOW_EXTERNAL", SDL_WINDOW_EXTERNAL)
+  V("SDL_WINDOW_MODAL", SDL_WINDOW_MODAL)
+  V("SDL_WINDOW_HIGH_PIXEL_DENSITY", SDL_WINDOW_HIGH_PIXEL_DENSITY)
+  V("SDL_WINDOW_MOUSE_CAPTURE", SDL_WINDOW_MOUSE_CAPTURE)
+  V("SDL_WINDOW_ALWAYS_ON_TOP", SDL_WINDOW_ALWAYS_ON_TOP)
+  V("SDL_WINDOW_UTILITY", SDL_WINDOW_UTILITY)
+  V("SDL_WINDOW_TOOLTIP", SDL_WINDOW_TOOLTIP)
+  V("SDL_WINDOW_POPUP_MENU", SDL_WINDOW_POPUP_MENU)
+  V("SDL_WINDOW_KEYBOARD_GRABBED", SDL_WINDOW_KEYBOARD_GRABBED)
+  V("SDL_WINDOW_VULKAN", SDL_WINDOW_VULKAN)
+  V("SDL_WINDOW_METAL", SDL_WINDOW_METAL)
+  V("SDL_WINDOW_TRANSPARENT", SDL_WINDOW_TRANSPARENT)
+  V("SDL_WINDOW_NOT_FOCUSABLE", SDL_WINDOW_NOT_FOCUSABLE)
 #undef V
 
 #define V(name, function) \
--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 const test = require('brittle')
 const sdl = require('.')
 
-test('it should expose window position center const', (t) => {
-  t.ok(sdl.constants.SDL_WINDOWPOS_CENTERED)
-})
-
 test('it should expose a window class', (t) => {
   const win = new sdl.Window('Bare-ly a Window', 100, 100)
   t.ok(win)
@@ -15,4 +11,9 @@ test('window class should be destroyed', (t) => {
   t.execution(() => {
     win.destroy()
   })
+})
+
+test('it should be possible to pass a flag', (t) => {
+  const win = new sdl.Window('Bare-ly a Window', 100, 100, sdl.constants.windowFlags.SDL_WINDOW_FULLSCREEN)
+  t.ok(win)
 })
