90250c5f076050f011793eb6f9408971798daff9
Tony Gorez <gorez.tony@gmail.com>
2025-05-28 10:37:45 +0100

    test(renderer): fix .present test
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/renderer.js
+++ test/renderer.js
@@ -33,7 +33,7 @@ test('Renderer class should expose a present method', (t) => {
   const win = new sdl.Window('test', 100, 100)
   const ren = new sdl.Renderer(win)
 
-  t.ok(ren.clear())
+  t.ok(typeof ren.present() == 'boolean')
 })
 
 test('Renderer class should expose a texture method', (t) => {
