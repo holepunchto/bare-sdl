347a94fa48705d1513b14d432a45b809d58ef3c4
Tony Gorez <gorez.tony@gmail.com>
2025-05-31 07:03:27 +0100

    test(renderer): add clean
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/renderer.js
+++ test/renderer.js
@@ -4,6 +4,10 @@ const sdl = require('..')
 test('it should expose a Renderer class', (t) => {
   const win = new sdl.Window('test', 100, 100)
   const ren = new sdl.Renderer(win)
+  t.teardown(() => {
+    clean(win, ren)
+  })
+
   t.ok(ren)
 })
 
@@ -13,25 +17,22 @@ test('Renderer class should throw if window is not a intance of Window', (t) => 
   })
 })
 
-test('Renderer class could be destroyed', (t) => {
-  const win = new sdl.Window('test', 100, 100)
-  const ren = new sdl.Renderer(win)
-
-  t.execution(() => {
-    // TODO: the destroy method is not called
-    ren._destroy()
-  })
-})
-
 test('Renderer class should expose a clear method', (t) => {
   const win = new sdl.Window('test', 100, 100)
   const ren = new sdl.Renderer(win)
+  t.teardown(() => {
+    clean(win, ren)
+  })
+
   t.ok(typeof ren.clear() == 'boolean')
 })
 
 test('Renderer class should expose a present method', (t) => {
   const win = new sdl.Window('test', 100, 100)
   const ren = new sdl.Renderer(win)
+  t.teardown(() => {
+    clean(win, ren)
+  })
 
   t.ok(typeof ren.present() == 'boolean')
 })
@@ -40,6 +41,17 @@ test('Renderer class should expose a texture method', (t) => {
   const win = new sdl.Window('test', 100, 100)
   const ren = new sdl.Renderer(win)
   const tex = new sdl.Texture(ren, 100, 100)
+  t.teardown(() => {
+    clean(win, ren, tex)
+  })
 
   t.ok(typeof ren.texture(tex) == 'boolean')
 })
+
+// Helpers
+
+function clean(win, ren, tex) {
+  if (tex) tex._destroy()
+  ren._destroy()
+  win._destroy()
+}
