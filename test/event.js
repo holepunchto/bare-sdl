77686030248f78a77245dc140dca5e0d24958452
Tony Gorez <gorez.tony@gmail.com>
2025-06-10 09:57:22 +0200

    refactor(event): make Event optional for KeyboardEvent
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/event.js
+++ test/event.js
@@ -12,11 +12,16 @@ test('Event class shouls expose a type getter initialized at zero', (t) => {
 })
 
 test('it should expose an Event.Keyboard class', (t) => {
-  const event = new sdl.Event.Keyboard(new sdl.Event())
+  const event = new sdl.Event.Keyboard()
   t.ok(event)
 })
 
 test('Event.Keyboard class shouls expose a scancode getter initialized at zero', (t) => {
-  const event = new sdl.Event.Keyboard(new sdl.Event())
+  const event = new sdl.Event.Keyboard()
   t.ok(event.scancode == 0)
+})
+
+test('Event.Keyboard should be initialized by passing an Event instance', (t) => {
+  const event = new sdl.Event.Keyboard(new sdl.Event())
+  t.ok(event)
 })
