87fbf0ca4e1614f1605031cd839d290a8bb66ebb
Tony Gorez <gorez.tony@gmail.com>
2025-06-10 09:30:48 +0200

    test: add keyboard event
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/event.js
+++ test/event.js
@@ -10,3 +10,13 @@ test('Event class shouls expose a type getter initialize at zero', (t) => {
   const event = new sdl.Event()
   t.ok(event.type == 0)
 })
+
+test('it should expose an Event.Keyboard class', (t) => {
+  const event = new sdl.Event.Keyboard(new sdl.Event())
+  t.ok(event)
+})
+
+test('Event.Keyboard class shouls expose a scancode getter', (t) => {
+  const code = new sdl.Event.Keyboard(new sdl.Event())
+  t.ok(code)
+})
