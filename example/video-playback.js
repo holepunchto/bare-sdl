a6028b7541ac1a1a8610e9a9d2bfe9bbe22335bc
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-06-23 15:08:06 +0200

    Adjust playback example

--- example/video-playback.js
+++ example/video-playback.js
@@ -1,16 +1,6 @@
 const ffmpeg = require('bare-ffmpeg')
 const sdl = require('bare-sdl')
 
-// Log
-
-function log(message) {
-  if (Bare.argv.includes('--debug')) {
-    console.log(message)
-  }
-}
-
-// Playback class
-
 class Playback {
   constructor(width, height) {
     this.win = new sdl.Window('Window', width, height)
@@ -26,9 +16,9 @@ class Playback {
   }
 
   destroy() {
-    this.win._destroy()
-    this.ren._destroy()
-    this.tex._destroy()
+    this.win.destroy()
+    this.ren.destroy()
+    this.tex.destroy()
   }
 
   render(buffer, lineSize) {
@@ -49,16 +39,18 @@ class Playback {
   }
 }
 
-// Set up device
 let url = undefined
-if (Bare.platform == 'win32') url = 'video=FaceTime HD Camera'
+if (Bare.platform === 'darwin') url = '0'
+if (Bare.platform === 'win32') url = 'video=FaceTime HD Camera'
 
 const options = new ffmpeg.Dictionary()
 options.set('framerate', '30')
 options.set('video_size', '1280x720')
-if (Bare.platform == 'darwin') options.set('pixel_format', 'uyvy422')
-if (Bare.platform == 'win32') options.set('pixel_format', 'yuyv422')
-if (Bare.platform == 'win32') options.set('rtbufsize', '100M')
+if (Bare.platform === 'darwin') options.set('pixel_format', 'uyvy422')
+if (Bare.platform === 'win32') {
+  options.set('pixel_format', 'yuyv422')
+  options.set('rtbufsize', '100M')
+}
 
 const inputFormatContext = new ffmpeg.InputFormatContext(
   new ffmpeg.InputFormat(),
@@ -66,108 +58,88 @@ const inputFormatContext = new ffmpeg.InputFormatContext(
   url
 )
 
-// Find video stream
 const bestStream = inputFormatContext.getBestStream(
   ffmpeg.constants.mediaTypes.VIDEO
 )
-if (!bestStream) {
-  process.exit(1)
-}
+if (!bestStream) process.exit(1)
 
-// Setup rawDecoder
-const rawDecoder = bestStream.decoder()
+const decoder = bestStream.decoder()
 
-// Set up codec
 const codec = ffmpeg.Codec.H264
 
-// Set up decoder
 const decoderContext = new ffmpeg.CodecContext(codec.decoder)
 decoderContext.open()
 
-// Set up encoder
 const encoderOptions = new ffmpeg.Dictionary()
 encoderOptions.set('preset', 'ultrafast')
 encoderOptions.set('tune', 'zerolatency')
+
 const encoderContext = new ffmpeg.CodecContext(codec.encoder)
-encoderContext.width = rawDecoder.width
-encoderContext.height = rawDecoder.height
+encoderContext.width = decoder.width
+encoderContext.height = decoder.height
 encoderContext.pixelFormat = ffmpeg.constants.pixelFormats.YUV420P
 encoderContext.timeBase = new ffmpeg.Rational(1, 30)
 encoderContext.open(encoderOptions)
 
-// Set up playback
-const playback = new Playback(rawDecoder.width, rawDecoder.height)
+const playback = new Playback(decoder.width, decoder.height)
 
-// Allocate frames
-const rawFrame = new ffmpeg.Frame()
-const yuvFrame = new ffmpeg.Frame()
-yuvFrame.width = rawDecoder.width
-yuvFrame.height = rawDecoder.height
-yuvFrame.pixelFormat = ffmpeg.constants.pixelFormats.YUV420P
-yuvFrame.alloc()
-const rgbFrame = new ffmpeg.Frame()
-rgbFrame.width = rawDecoder.width
-rgbFrame.height = rawDecoder.height
-rgbFrame.pixelFormat = ffmpeg.constants.pixelFormats.RGB24
-rgbFrame.alloc()
-const rgbaImage = new ffmpeg.Image(
+const raw = new ffmpeg.Frame()
+
+const yuv = new ffmpeg.Frame()
+yuv.width = decoder.width
+yuv.height = decoder.height
+yuv.pixelFormat = ffmpeg.constants.pixelFormats.YUV420P
+yuv.alloc()
+
+const rgb = new ffmpeg.Frame()
+rgb.width = decoder.width
+rgb.height = decoder.height
+rgb.pixelFormat = ffmpeg.constants.pixelFormats.RGB24
+rgb.alloc()
+
+const image = new ffmpeg.Image(
   ffmpeg.constants.pixelFormats.RGBA,
-  rgbFrame.width,
-  rgbFrame.height
+  rgb.width,
+  rgb.height
 )
-rgbaImage.fill(rgbFrame)
+image.fill(rgb)
 
-// Set up toYUV  scaler
 const toYUV = new ffmpeg.Scaler(
-  rawDecoder.pixelFormat,
-  rawDecoder.width,
-  rawDecoder.height,
+  decoder.pixelFormat,
+  decoder.width,
+  decoder.height,
   ffmpeg.constants.pixelFormats.YUV420P,
-  yuvFrame.width,
-  yuvFrame.height
+  yuv.width,
+  yuv.height
 )
 
-// Set up toRGB scaler
 const toRGB = new ffmpeg.Scaler(
   ffmpeg.constants.pixelFormats.YUV420P,
-  rawDecoder.width,
-  rawDecoder.height,
+  decoder.width,
+  decoder.height,
   ffmpeg.constants.pixelFormats.RGB24,
-  rawDecoder.width,
-  rawDecoder.height
+  decoder.width,
+  decoder.height
 )
 
 function encode(packet) {
-  const ret = inputFormatContext.readFrame(packet)
-  if (!ret) return
+  const result = inputFormatContext.readFrame(packet)
+  if (!result) return
 
-  rawDecoder.sendPacket(packet)
+  decoder.sendPacket(packet)
   packet.unref()
 
-  while (rawDecoder.receiveFrame(rawFrame)) {
-    log('1 - decoded frame')
-    // NOTE: for Mafintosh
-    // This were you can playback for the sender
-    // playback.render(rawFrame)
-
-    toYUV.scale(rawFrame, yuvFrame)
-    log('2 - scale frame to yuv')
-
-    encoderContext.sendFrame(yuvFrame)
-    log('3 - send frame')
+  while (decoder.receiveFrame(raw)) {
+    toYUV.scale(raw, yuv)
+    encoderContext.sendFrame(yuv)
 
     while (encoderContext.receivePacket(packet)) {
-      log('4 - encoded packet')
-      // NOTE: for Mafintosh
-      // This where you push to the swarm!
       decode(packet.data)
       packet.unref()
     }
   }
 }
 
-// NOTE: for Mafintosh
-// This the function that you could use on the receiver side
 function decode(buffer) {
   const packet = new ffmpeg.Packet(buffer)
   decoderContext.sendPacket(packet)
@@ -175,10 +147,8 @@ function decode(buffer) {
 
   const decodedFrame = new ffmpeg.Frame()
   while (decoderContext.receiveFrame(decodedFrame)) {
-    log('5 - decoded frame')
-    log('6 - scale frame to rgba')
-    toRGB.scale(decodedFrame, rgbFrame)
-    playback.render(rgbaImage.data, rgbaImage.lineSize())
+    toRGB.scale(decodedFrame, rgb)
+    playback.render(image.data, image.lineSize())
   }
 }
 
